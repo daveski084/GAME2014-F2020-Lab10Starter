8c0168163baeb17de8dccf25853fd4698792eb7f
David <daveski084@users.noreply.github.com>
2020-12-02 18:58:48 -0500

    added dust trail

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -32,6 +32,10 @@ public class PlayerBehaviour : MonoBehaviour
     public BarController healthBar;
     public Animator livesHUD;
 
+    [Header("Dust Trail")]
+    public ParticleSystem dustTrail;
+    public Color dustTrailColor;
+
     private Rigidbody2D m_rigidBody2D;
     private SpriteRenderer m_spriteRenderer;
     private Animator m_animator;
@@ -46,6 +50,7 @@ public class PlayerBehaviour : MonoBehaviour
         m_rigidBody2D = GetComponent<Rigidbody2D>();
         m_spriteRenderer = GetComponent<SpriteRenderer>();
         m_animator = GetComponent<Animator>();
+        dustTrail = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -124,7 +129,7 @@ public class PlayerBehaviour : MonoBehaviour
                         m_rigidBody2D.AddForce(Vector2.down * horizontalForce * rampForceFactor * Time.deltaTime);
                     }
 
-
+                    CreateDustTrail();
                     m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
                 }
                 else if (joystick.Horizontal < -joystickHorizontalSensitivity)
@@ -141,6 +146,7 @@ public class PlayerBehaviour : MonoBehaviour
                         m_rigidBody2D.AddForce(Vector2.down * horizontalForce * 0.5f * Time.deltaTime);
                     }
 
+                    CreateDustTrail();
                     m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
                 }
                 else
@@ -155,6 +161,7 @@ public class PlayerBehaviour : MonoBehaviour
                 m_rigidBody2D.AddForce(Vector2.up * verticalForce);
                 m_animator.SetInteger("AnimState", (int) PlayerAnimationType.JUMP);
                 isJumping = true;
+                CreateDustTrail();
             }
             else
             {
@@ -224,5 +231,11 @@ public class PlayerBehaviour : MonoBehaviour
         {
             LoseLife();
         }
+    }
+
+    private void CreateDustTrail()
+    {
+        dustTrail.GetComponent<Renderer>().material.SetColor("Color", dustTrailColor);
+        dustTrail.Play();
     }
 }
